696e91cb36eb04ab98179c239df3c31ff5d8aaa2
apurbzz-1994 <akash95august@gmail.com>
2020-10-25 13:33:07 +1100

    add script file and FriendList class

---
+++ friendList.js
@@ -0,0 +1,25 @@
+class FriendList{
+    constructor(){
+        this._friendList = [];
+    }
+
+    // return the friendList array
+    get friendList(){
+        return this._friendList;
+    }
+
+    // returns the length of friendList array
+    get friendCount(){
+        return this._friendList.length;
+    }
+
+    addFriend(id){
+        let aNewFriend = new Friend(id);
+        this._friendList.push(aNewFriend);
+    }
+
+    // returns a Friend object at the index provided
+    getFriend(fIndex){
+        return this._friendList[fIndex];
+    }
+}
